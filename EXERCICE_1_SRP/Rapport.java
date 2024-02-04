c1cbb64d14abd7b541a617b1e4d16cd9e5b55809
cherif <you@example.com>
2024-02-04 20:00:48 +0100

    SOLID PRINCIPLE V5

--- EXERCICE_1_SRP/Rapport.java
+++ EXERCICE_1_SRP/Rapport.java
@@ -1,8 +1,9 @@
 package EXERCICE_1_SRP;
 
 public class Rapport {
-    public void printTimeSheetReport(Employee employee) {
-        System.out.println("Time sheet report for employee: " + employee.getName());
+    public void printTimeSheetReport(EmployeeSet employeesSet) {
+        EmployeeSet employeeSet = new EmployeeSet(null);
+        System.out.println("Time sheet report for employee: " + employeeSet.getName());
     }
 
 }
